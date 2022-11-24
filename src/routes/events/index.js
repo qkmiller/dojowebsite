05944f1cf2cf54e79dc655145945636711e1016c
abaduser <Isaac.K.Masood@gmail.com>
2022-11-23 17:24:00 -0800

    Updated Events
    Added rules maps etc, added signup page

--- src/routes/events/index.js
+++ src/routes/events/index.js
@@ -4,70 +4,60 @@ import style from './style.css';
 const Events = () => (
 	<div class={style.events}>
 		<h1>The Dojo's Beginner's 2022 Fall Cup</h1>
-		<p>Sign up Now!: </p>
+		<p>Sign up Now!: <a href="https://challonge.com/tournaments/signup/KjDBg8GecY#/signup/aszw2ltb9a">Click Here</a></p>
 		
-		<h1>Rules</h1>
+		<h1>Info</h1>
+		<p>The Tournament will be held on the weekend of the 26th and extend until all matches are complete.</p>
+		<p>Tournament will be double elimination, with seeding according to belt/elo. Best of Threes will be standard until the Semi-final Best of Fives. The Grand Finals will be Best Of Seven.</p> 
+		<p>For Best Of Threes: The first map will be chosen by the coinflip winner, second by the opponent. The third map will be randomized, and the lower seed will get server preference.</p>
 		<h2>Map Pick Rules</h2>
-		<p>Each player will pick 8 of their favorite maps from the pool and have them in their own personal map pool. As you move through the tournament you will only be able to use each map from your pool once.</p>
-		<p>Once players make it to the semi-finals, they will be able to pick any previously picked map, as well as be given a ban.</p>
+		<p>Each player will pick a map for their best of three set with their opponent. As you move through the tournament you will </p>
+		<p>Once players make it to the semi-finals, they will be able to pick any previously picked maps, as well as be given 3 bans for the tiebreaker pool.</p>
+		<p>The first player to ban will be chosen via a coin toss.</p>
+		<h2><u>TIE BREAKER MAP POOL</u></h2>
+		<ul>
+			<h3>
+			<li>Solitude</li>
+			<li>Refinery</li>
+			<li>McSarges</li>
+			<li>Battleforged</li>
+			<li>House of decay</li>
+			<li>Sorrow</li>
+			<li>Furiousheights</li>
+			</h3>
+		</ul>
 		<h2><u>MAP POOL</u></h2>
-<ul>
-<li>aerowalk</li>
-<br></br>
-<li>almostlost</li>
-<br></br>
-<li>battleforged</li>
-<br></br>
-<li>bitter embrace</li>
-<br></br>
-<li>bloodrun</li>
-<br></br>
-<li>cure</li>
-<br></br>
-<li>delirium</li>
-<br></br>
-<li>devilish</li>
-<br></br>
-<li>dismemberment</li>
-<br></br>
-<li>elder</li>
-<br></br>
-<li>furriousheights</li>
-<br></br>
-<li>hektik</li>
-<br></br>
-<li>houseofdecay</li>
-<br></br>
-<li>lostworld</li>
-<br></br>
-<li>mcsarges</li>
-<br></br>
-<li>phrantic</li>
-<br></br>
-<li>refinery</li>
-<br></br>
-<li>silence</li>
-<br></br>
-<li>sinister</li>
-<br></br>
-<li>solarium</li>
-<br></br>
-<li>sorrow</li>
-<br></br>
-<li>terminatria</li>
-<br></br>
-<li>theedge</li>
-<br></br>
-<li>toxic</li>
-<br></br>
-<li>trinity</li>
-<br></br>
-<li>use and abuse</li>
-<br></br>
-<li>wicked</li>
-<br></br>
-<li>oilrig</li>
-<br></br>
+			<ul>
+			<li>aerowalk</li>
+			<li>almostlost</li>
+			<li>battleforged</li>
+			<li>bitter embrace</li>
+			<li>bloodrun</li>
+			<li>campgrounds</li>
+			<li>cure</li>
+			<li>delirium</li>
+			<li>devilish</li>
+			<li>dismemberment</li>
+			<li>elder</li>
+			<li>furriousheights</li>
+			<li>hektik</li>
+			<li>houseofdecay</li>
+			<li>lostworld</li>
+			<li>mcsarges</li>
+			<li>oilrig</li>
+			<li>phrantic</li>
+			<li>refinery</li>
+			<li>silence</li>
+			<li>sinister</li>
+			<li>solarium</li>
+			<li>solitude</li>
+			<li>sorrow</li>
+			<li>terminatria</li>
+			<li>theedge</li>
+			<li>toxic</li>
+			<li>trinity</li>
+			<li>usedandabused</li>
+			<li>wicked</li>
 </ul>
 		<h2>Etiquette</h2>
 		<p>Everyone is expected to act in a professional manner. Any rule breakers will be banned from the Quake Servers as well as the Discord</p>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,8 +4,8 @@ import style from './style.css';
 
 const Home = () => (	
 	<div class={style.home}>
-		<h1>Lorem Ipsum</h1>
-		<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+		<h1>Welcome to the Dojo</h1>
+		<p>We are a community of Quake Live players who focus on Arena FPS and becoming better players. We are located in North America and play on our own servers, teaching and educating students of Duel. We host our own tournaments to encourage player growth, and seek to create a positive respectful, egalitarian duel environment.</p>
 		<br></br>
 	</div>
 );
