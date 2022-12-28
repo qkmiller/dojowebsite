e4664677f3fc2c041c3742dc144331ea59937260
abaduser <Isaac.K.Masood@gmail.com>
2022-12-28 06:08:10 -0800

    intermediate rules and style

--- src/routes/events/index.js
+++ src/routes/events/index.js
@@ -3,66 +3,32 @@ import style from './style.css';
 
 const Events = () => (
 	<div class={style.events}>
-		<h1>The Dojo's Beginner's 2022 Fall Cup</h1>
-		<p>Sign up Now!: <a href="https://challonge.com/tournaments/signup/KjDBg8GecY#/signup/aszw2ltb9a">Click Here</a></p>
-		<h1>Info</h1>
-		<p>The Tournament will be held on the weekend of the 26th and extend until all matches are complete.</p>
-		<p>Tournament will be double elimination, with seeding according to belt/elo. Best of Threes will be standard until the Semi-final Best of Fives. The Grand Finals will be Best Of Seven.</p> 
-		<p>For Best Of Threes: The first map will be chosen by the highest seed, second by the opponent. The third map will be randomized</p>
-		<p>Each player can agree to play on their own servers respectively for each match, and then coinflip for the final server location <b>otherwise,</b> a middle server will be picked by admins.</p>
-		<h2>Map Pick Rules</h2>
-		<p>Each player will pick a map for their best of three set with their opponent. As you move through the tournament you will not be able to pick the map you used to win the set, again in the tournament.</p>
-		<p>If you are sent to the losers bracket you can re-use maps.</p>
-		<p>Once players make it to the semi-finals, they will be able to pick any previously picked maps, as well as be given 3 bans for the tiebreaker pool.</p>
-		<p>The first player to ban will be chosen via a coin toss.</p>
-		<h2><u>TIE BREAKER MAP POOL</u></h2>
+		<h1>The Dojo's Intermediate's 2023 Spring Cup</h1>
+		<h3>Rules:</h3>
+		<p>
+			Double elimination B-O-3, semi finals B-O-5, and finals B-O-7.
+			Players each will pick their respective number of maps, then ban the rest of the list for a tiebreaker.
+        <p>
+            A 5$ Enterance Fee will be required to participate in the Intermediate's.
+        </p>
+		</p>
+		<br></br>
+		<h3>Time:</h3>
+		<p>
+			To be announced, A vote will be held to determine an early Spring Date. The tournament will be held on one day. Players will be expected to play anywhere from 1-6 hours with breaks.
+			</p><p><u><b>No shows (within 1 hour of start) will be disqualified immediately and their entry fee not refunded.</b></u>
+		</p>
+		<br></br>
+		<h2>Map Pool</h2>
 		<ul>
-			<h3>
-			<li>Solitude</li>
-			<li>Refinery</li>
-			<li>McSarges</li>
-			<li>Battleforged</li>
-			<li>House of decay</li>
-			<li>Sorrow</li>
-			<li>Furious heights</li>
-			</h3>
+			<li>Aerowalk</li>
+			<li>Toxicity</li>
+			<li>Bloodrun</li>
+			<li>Sinister</li>
+			<li>Cure</li>
+			<li>Elder</li>
+			<li>Delerium</li>
 		</ul>
-		<h2><u>MAP POOL</u></h2>
-			<ul>
-			<li>aerowalk</li>
-			<li>almost lost</li>
-			<li>battleforged</li>
-			<li>bitter embrace</li>
-			<li>bloodrun</li>
-			<li>campgrounds</li>
-			<li>cure</li>
-			<li>delirium</li>
-			<li>devilish</li>
-			<li>dismemberment</li>
-			<li>elder</li>
-			<li>furrious heights</li>
-			<li>fuse</li>
-			<li>hektik</li>
-			<li>house of decay</li>
-			<li>lost world</li>
-			<li>mcsarges</li>
-			<li>oilrig</li>
-			<li>phrantic</li>
-			<li>refinery</li>
-			<li>silence</li>
-			<li>sinister</li>
-			<li>solarium</li>
-			<li>solitude</li>
-			<li>sorrow</li>
-			<li>terminatria</li>
-			<li>the edge</li>
-			<li>toxic</li>
-			<li>trinity</li>
-			<li>used and abused</li>
-			<li>wicked</li>
-		</ul>
-		<h2>Etiquette</h2>
-		<p>Everyone is expected to act in a professional manner. Any rule breakers will be banned from the Quake Servers as well as the Discord</p>
 	</div>
 )
 
